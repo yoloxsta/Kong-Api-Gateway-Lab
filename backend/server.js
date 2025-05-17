b729263db55ae0a3476e5dd248d337784b579f13
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-05-17 21:34:41 +0630

    code push

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World from Backend Service!");
+});
+
+app.listen(port, () => {
+  console.log(`Backend service listening at http://localhost:${port}`);
+});
