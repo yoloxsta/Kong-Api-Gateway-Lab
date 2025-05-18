826c50776798ee1ceb78684ac5e92de4a799a127
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-05-18 22:36:50 +0630

    code fix

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000; <-- 3003
+const port = 3000; 
 app.get("/", (req, res) => {
   res.send("Hello World from Backend Service(konggggg)");
 });
